224ef5ff2d4ed0e5e8b0c1a6dbd1537c47f0cdb5
William Sapsford <sapsfw@amazon.com>
2023-11-08 11:59:01 +0000

    First hackathon contract update

--- contract/scripts/deploy.sh
+++ contract/scripts/deploy.sh
@@ -5,4 +5,4 @@ cargo build --all --target wasm32-unknown-unknown --release
 pwd
 cp target/wasm32-unknown-unknown/release/*.wasm ./res/
 
-near deploy dev-1698625307858-61130313402632 ./res/obrigado_is_near.wasm
+near deploy obrigado.testnet ./res/obrigado_is_near.wasm
--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,43 +1,19 @@
+use core::fmt;
+
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::Vector;
+use near_sdk::json_types::U128;
 use near_sdk::{env, near_bindgen, AccountId, Balance, PanicOnDefault};
-use near_sdk::collections::{Vector, UnorderedMap};
-use near_sdk::json_types::{U128, Base64VecU8};
-use near_sdk::serde_json;
-use std::collections::HashMap;
 
-use bs58;
+use rand::Rng;
 
-// Add the sha2 crate to your Cargo.toml dependencies
-use sha2::{Sha256, Digest};
+const MINIMUM_COMPUTE_COUNT: U128 = U128(3);
+const TASK_ID_LENGTH: u128 = 10;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct TaskContract {
-    tasks: UnorderedMap<u64, Task>,
-    task_results: UnorderedMap<u64, Vector<TaskResult>>,
-    task_count: u64,
-}
-
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct Task {
-    bounty: Balance,
-    workspace_url: String,
-    status: TaskStatus,
-    submitter: AccountId,
-}
-
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct TaskResult {
-    result_url: String,
-    result_hash: Base64VecU8,
-    submitter: AccountId,
-}
-
-#[derive(BorshDeserialize, BorshSerialize)]
-pub enum TaskStatus {
-    Open,
-    Completed,
-    Challenged,
+    task_queue: Vector<Task>,
 }
 
 #[near_bindgen]
@@ -45,86 +21,181 @@ impl TaskContract {
     #[init]
     pub fn new() -> Self {
         Self {
-            tasks: UnorderedMap::new(b"t".to_vec()),
-            task_results: UnorderedMap::new(b"r".to_vec()),
-            task_count: 0,
+            task_queue: Vector::new(b"t"),
         }
     }
 
-    pub fn add_task(&mut self, bounty: U128, workspace_url: String) -> u64 {
-        let task_id = self.task_count;
-        let task = Task {
-            bounty: bounty.0,
-            workspace_url,
-            status: TaskStatus::Open,
-            submitter: env::signer_account_id(),
-        };
-        self.tasks.insert(&task_id, &task);
-        self.task_count += 1;
-        env::log_str(&format!("Task {} added by {}", task_id, task.submitter));
-        task_id
+    pub fn create_task(&mut self, bounty: Balance, repository_url: String) -> String {
+        let task = Task::new(bounty, repository_url);
+
+        self.task_queue.push(&task);
+
+        format!("Task created successfully: {}", task)
     }
 
-    #[payable]
-    pub fn submit_task_result(&mut self, task_id: u64, result_url: String, result_hash: Base64VecU8) {
-        let deposit = env::attached_deposit();
-        let task = self.tasks.get(&task_id).expect("Task not found");
+    //pub fn submit_task_result(&mut self, )
 
-        // Check if the deposit is at least 30% of the bounty
-        assert!(deposit >= task.bounty / 3, "Deposit must be at least 30% of the bounty");
+    // #[payable]
+    // pub fn submit_task_result(
+    //     &mut self,
+    //     task_id: u64,
+    //     result_url: String,
+    //     result_hash: Base64VecU8,
+    // ) {
+    //     let deposit = env::attached_deposit();
+    //     let task = self.tasks.get(&task_id).expect("Task not found");
 
-        // Check if the task is still open
-        assert!(matches!(task.status, TaskStatus::Open), "Task is not open");
+    //     // Check if the deposit is at least 30% of the bounty
+    //     assert!(
+    //         deposit >= task.bounty / 3,
+    //         "Deposit must be at least 30% of the bounty"
+    //     );
 
-        // Check if the submitter has already submitted a result for this task
-        let results = self.task_results.get(&task_id).unwrap_or_else(|| Vector::new(b"r".to_vec()));
-        assert!(
-            results.iter().all(|r| r.submitter != env::signer_account_id()),
-            "Submitter has already submitted a result for this task"
-        );
+    //     // Check if the task is still open
+    //     assert!(matches!(task.status, TaskStatus::Open), "Task is not open");
 
-        // Add the result
-        let mut results = results;
-        results.push(&TaskResult {
-            result_url,
-            result_hash,
-            submitter: env::signer_account_id(),
-        });
-        self.task_results.insert(&task_id, &results);
+    //     // Check if the submitter has already submitted a result for this task
+    //     let results = self
+    //         .task_results
+    //         .get(&task_id)
+    //         .unwrap_or_else(|| Vector::new(b"r".to_vec()));
+    //     assert!(
+    //         results
+    //             .iter()
+    //             .all(|r| r.submitter != env::signer_account_id()),
+    //         "Submitter has already submitted a result for this task"
+    //     );
 
-        // Check if the task is completed
-        // Convert Base64VecU8 to String before using it as a key
-        let mut result_count: HashMap<String, u64> = HashMap::new();
-        for result in results.iter() {
-            // Access the underlying Vec<u8> directly with .0
-            let hash_string = bs58::encode(result.result_hash.0.as_slice()).into_string();
-            *result_count.entry(hash_string).or_insert(0) += 1;
-        }
-        
+    //     // Add the result
+    //     let mut results = results;
+    //     results.push(&TaskResult {
+    //         result_url,
+    //         result_hash,
+    //         submitter: env::signer_account_id(),
+    //     });
+    //     self.task_results.insert(&task_id, &results);
 
-        if let Some(count) = result_count.values().find(|&&count| count >= 3) {
-            // Task is completed
-            self.tasks.insert(&task_id, &Task {
-                status: TaskStatus::Completed,
-                ..task
-            });
-            env::log_str(&format!("Task {} completed", task_id));
+    //     // Check if the task is completed
+    //     // Convert Base64VecU8 to String before using it as a key
+    //     let mut result_count: HashMap<String, u64> = HashMap::new();
+    //     for result in results.iter() {
+    //         // Access the underlying Vec<u8> directly with .0
+    //         let hash_string = bs58::encode(result.result_hash.0.as_slice()).into_string();
+    //         *result_count.entry(hash_string).or_insert(0) += 1;
+    //     }
+
+    //     if let Some(count) = result_count.values().find(|&&count| count >= 3) {
+    //         // Task is completed
+    //         self.tasks.insert(
+    //             &task_id,
+    //             &Task {
+    //                 status: TaskStatus::Completed,
+    //                 ..task
+    //             },
+    //         );
+    //         env::log_str(&format!("Task {} completed", task_id));
+    //     }
+    // }
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Task {
+    submitter_account_id: AccountId,
+    bounty: Balance,
+    repository_url: String,
+    compute_count: U128,
+    id: String,
+}
+
+impl Task {
+    fn new(bounty: Balance, repository_url: String) -> Self {
+        Self {
+            submitter_account_id: env::signer_account_id(),
+            bounty,
+            repository_url,
+            compute_count: U128(0),
+            id: generate_task_id(),
         }
     }
 }
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{{ submitter_account_id: {}, bounty: {}, respository_url: {}, compute_count: {} }}",
+            self.submitter_account_id,
+            self.bounty,
+            self.repository_url,
+            u128::from(self.compute_count)
+        )
+    }
+}
+
+// fn generate_task_id(bounty: &Balance, repository_url: String) {
+//     let mut hash = DefaultHasher::new();
+//     let seed = env::random_seed();
+//     let timestamp = env::block_timestamp();
+//     let block_height = env::block_height();
+
+//     repository_url.hash(&mut hash);
+//     contract_len.hash(&mut hash);
+//     seed.hash(&mut hash);
+//     timestamp.hash(&mut hash);
+//     block_height.hash(&mut hash);
+
+//     hash.finish().to_string()
+// }
+
+// fn generate_sha256_hash() {
+//     let seed = env::random_seed();
+//     let timestamp = env::block_timestamp();
+//     let block_height = env::block_height();
+
+//     let mut hasher = Sha256::new();
+
+//     hasher.update(seed.as_)
+// }
+
+fn generate_task_id() -> String {
+    let charset: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let mut rng = rand::thread_rng();
+    let random_string: String = (0..TASK_ID_LENGTH)
+        .map(|_| {
+            let index = rng.gen_range(0..charset.len());
+            charset[index] as char
+        })
+        .collect();
+
+    format!("{}-{}", env::signer_account_id(), random_string)
+}
+
+// #[derive(BorshDeserialize, BorshSerialize)]
+// pub struct TaskResult {
+//     creator_account_id: AccountId,
+//     url: String,
+//     hash: Base64VecU8,
+// }
+
+// #[derive(BorshDeserialize, BorshSerialize)]
+// pub enum TaskStatus {
+//     Open,
+//     Completed,
+//     Challenged,
+// }
 
 // Helper function to hash the result data
-fn hash_result_data(data: &[u8]) -> Base64VecU8 {
-    let mut hasher = Sha256::new();
-    hasher.update(data);
-    let result = hasher.finalize();
-    Base64VecU8::from(result.to_vec())
-}
+// fn hash_result_data(data: &[u8]) -> Base64VecU8 {
+//     let mut hasher = Sha256::new();
+//     hasher.update(data);
+//     let result = hasher.finalize();
+//     Base64VecU8::from(result.to_vec())
+// }
 
 // Helper function to verify a result hash
-fn verify_result_hash(data: &Vec<u8>, hash: &Base64VecU8) -> bool {
-    hash_result_data(data) == *hash
-}
+// fn verify_result_hash(data: &Vec<u8>, hash: &Base64VecU8) -> bool {
+//     hash_result_data(data) == *hash
+// }
 
 // The rest of the contract would include more functions to manage tasks and results,
 // such as getting tasks, getting results for a task, challenging results, etc.
