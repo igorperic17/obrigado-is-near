6f8558711859175038521b653c70f5c8e8de30dc
Igor Peric <igorperic@live.com>
2023-11-09 13:28:33 +0000

    infura

--- worker-node/app.js
+++ worker-node/app.js
@@ -8,11 +8,6 @@ import { exec } from 'child_process';
 import { env } from 'process';
 import { promises as fs2 } from 'fs';
 
-const projectId = "2Xv5JJtzWWVdnZnA6ZMg9wo6Uif";
-const projectSecret = "b9752e5f1a396a1c3ba2277951427b10";
-
-const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
-
 const home_dir = env.HOME;
 const keyStore = new keyStores.UnencryptedFileSystemKeyStore(`${home_dir}/.near-credentials`);
 const nearConfig = {
@@ -127,6 +122,10 @@ async function uploadResultsToIPFS(workspaceDir, ipfs) {
       // Read the zipped file asynchronously
       const file = await fs2.readFile(zippedFilePath);
       console.log("about to upload");
+      
+      const projectId = "2Xv5JJtzWWVdnZnA6ZMg9wo6Uif";
+      const projectSecret = "b9752e5f1a396a1c3ba2277951427b10";
+      const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
       const ipfs = create({ host: 'ipfs.infura.io', port: 5001, protocol: 'https', headers: { authorization: auth }});
       const added = await ipfs.add(file);
       console.log("done uploading");
